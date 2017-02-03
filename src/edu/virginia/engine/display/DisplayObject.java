243b7e5e5e3ffae2d3f1b3cdf92870bd0037fe3a
Laith Hasanian <Laith@d-172-25-203-230.dhcp.virginia.edu>
2017-02-03 11:22:57 -0500

    first commit

---
+++ src/edu/virginia/engine/display/DisplayObject.java
@@ -0,0 +1,226 @@
+package edu.virginia.engine.display;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Point2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
+
+/**
+ * A very basic display object for a java based gaming engine
+ * 
+ * */
+public class DisplayObject {
+
+	/* All DisplayObject have a unique id */
+	private String id;
+
+	/* The image that is displayed by this object */
+	private BufferedImage displayImage;
+
+	//should be true iff this object is meant to be drawn
+	private boolean visible;
+
+	//x, y position where object will be drawn
+	private Point2D position;
+
+	//Point relative to upper left corner that is the origin of the object
+	private Point2D pivotPoint;
+
+	//scale
+	private double scaleX;
+	private double scaleY;
+
+	//degrees of rotation
+	private int rotation;
+
+	//transparency
+	private double alpha;
+
+	/**
+	 * Constructors: can pass in the id OR the id and image's file path and
+	 * position OR the id and a buffered image and position
+	 */
+	public DisplayObject(String id) {
+		this.setId(id);
+	}
+
+	public DisplayObject(String id, String fileName) {
+		this.setId(id);
+		this.setImage(fileName);
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+
+	/**
+	 * Returns the unscaled width and height of this display object
+	 * */
+	public int getUnscaledWidth() {
+		if(displayImage == null) return 0;
+		return displayImage.getWidth();
+	}
+
+	public int getUnscaledHeight() {
+		if(displayImage == null) return 0;
+		return displayImage.getHeight();
+	}
+
+	public BufferedImage getDisplayImage() {
+		return this.displayImage;
+	}
+
+	protected void setImage(String imageName) {
+		if (imageName == null) {
+			return;
+		}
+		displayImage = readImage(imageName);
+		if (displayImage == null) {
+			System.err.println("[DisplayObject.setImage] ERROR: " + imageName + " does not exist!");
+		}
+	}
+
+	public boolean isVisible() {
+		return visible;
+	}
+
+	public void setVisible(boolean visible) {
+		this.visible = visible;
+	}
+
+	public Point2D getPosition() {
+		return position;
+	}
+
+	public void setPosition(Point2D position) {
+		this.position = position;
+	}
+
+	public Point2D getPivotPoint() {
+		return pivotPoint;
+	}
+
+	public void setPivotPoint(Point2D pivotPoint) {
+		this.pivotPoint = pivotPoint;
+	}
+
+	public double getScaleX() {
+		return scaleX;
+	}
+
+	public void setScaleX(double scaleX) {
+		this.scaleX = scaleX;
+	}
+
+	public double getScaleY() {
+		return scaleY;
+	}
+
+	public void setScaleY(double scaleY) {
+		this.scaleY = scaleY;
+	}
+
+	public int getRotation() {
+		return rotation;
+	}
+
+	public void setRotation(int rotation) {
+		this.rotation = rotation;
+	}
+
+	public double getAlpha() {
+		return alpha;
+	}
+
+	public void setAlpha(double alpha) {
+		this.alpha = alpha;
+	}
+
+
+	/**
+	 * Helper function that simply reads an image from the given image name
+	 * (looks in resources\\) and returns the bufferedimage for that filename
+	 * */
+	public BufferedImage readImage(String imageName) {
+		BufferedImage image = null;
+		try {
+			String file = ("resources" + File.separator + imageName);
+			image = ImageIO.read(new File(file));
+		} catch (IOException e) {
+			System.out.println("[Error in DisplayObject.java:readImage] Could not read image " + imageName);
+			e.printStackTrace();
+		}
+		return image;
+	}
+
+	public void setImage(BufferedImage image) {
+		if(image == null) return;
+		displayImage = image;
+	}
+
+
+	/**
+	 * Invoked on every frame before drawing. Used to update this display
+	 * objects state before the draw occurs. Should be overridden if necessary
+	 * to update objects appropriately.
+	 * */
+	protected void update(ArrayList<String> pressedKeys) {
+		
+	}
+
+	/**
+	 * Draws this image. This should be overloaded if a display object should
+	 * draw to the screen differently. This method is automatically invoked on
+	 * every frame.
+	 * */
+	public void draw(Graphics g) {
+		
+		if (displayImage != null) {
+			
+			/*
+			 * Get the graphics and apply this objects transformations
+			 * (rotation, etc.)
+			 */
+			Graphics2D g2d = (Graphics2D) g;
+			applyTransformations(g2d);
+
+			/* Actually draw the image, perform the pivot point translation here */
+			g2d.drawImage(displayImage, 0, 0,
+					(int) (getUnscaledWidth()),
+					(int) (getUnscaledHeight()), null);
+			
+			/*
+			 * undo the transformations so this doesn't affect other display
+			 * objects
+			 */
+			reverseTransformations(g2d);
+		}
+	}
+
+	/**
+	 * Applies transformations for this display object to the given graphics
+	 * object
+	 * */
+	protected void applyTransformations(Graphics2D g2d) {
+	
+	}
+
+	/**
+	 * Reverses transformations for this display object to the given graphics
+	 * object
+	 * */
+	protected void reverseTransformations(Graphics2D g2d) {
+
+	}
+
+}
